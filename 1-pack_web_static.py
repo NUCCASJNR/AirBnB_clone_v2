df5039e76368a3444b5c96e0fd5e7cbe38e23707
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-05 15:35:55 +0100

    try this for task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents
+"""Fabric script that generates a .tgz archive from the contents
 of the web_static folder of your AirBnB Clone repo,
 using the function do_pack.
 
