2d6ca23f03041ef9f0eec48d92b830e07c55df26
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-07 15:18:48 +0100

    check

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,7 +14,9 @@ def do_deploy(archive_path):
     """Args:
             archive_path: path to the archive file
     """
-    if os.path.exists(archive_path):
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
         put(archive_path, '/tmp/')
         splited = archive_path.split(".")[0].split('/')[-1]
         run('sudo mkdir -p /data/web_static/releases/{}'.format(splited))
@@ -30,5 +32,5 @@ def do_deploy(archive_path):
                 /data/web_static/current")
         print(f"New version deployed!")
         return True
-    else:
+    except:
         return False
