3e5902d9a39c01b2d5a95b499e872eaf5782cbaf
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-07 22:25:28 +0100

    false

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,6 @@
 import os
 from fabric.api import *
 
-
 env.hosts = ['54.87.207.177', '18.209.180.49']
 env.user = "ubuntu"
 env.password = os.environ['password']
@@ -15,17 +14,25 @@ def do_deploy(archive_path):
 
     if not os.path.exists(archive_path):
         return False
-        put(archive_path, '/tmp/')
+    if put(archive_path, '/tmp/').failed:
+        return False
         splited = archive_path.split(".")[0].split("/")[-1]
-        run("sudo mkdir -p /data/web_static/releases/{}".format(splited))
-        run("sudo tar -xzvf /tmp/{}.tgz -C\
-                /data/web_static/releases/{}/".format(splited, splited))
-        run("sudo rm /tmp/{}.tgz".format(splited))
-        run("sudo mv /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}".format(splited, splited))
-        run("sudo rm -rf /data/web_static/releases/\
-                {}/web_static/".format(splited))
-        run(f"sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}\
-                /data/web_static/current".format(splited))
+    if run("sudo mkdir -p /data/web_static/releases/{}".format(splited)):
+        return False
+    if run("sudo tar -xzvf /tmp/{}.tgz -C\
+                /data/web_static/releases/{}/".format(splited, splited)):
+        return False
+    if run("sudo rm /tmp/{}.tgz".format(splited)):
+        return False
+    if run("sudo mv /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}".format(splited, splited)):
+        return False
+    if run("sudo rm -rf /data/web_static/releases/\
+            {}/web_static/".format(splited)):
+        return False
+    if run(f"sudo rm -rf /data/web_static/current"):
+        return False
+    if run("sudo ln -s /data/web_static/releases/{}\
+            /data/web_static/current".format(splited)):
+        return False
     return True
