8a4dc1a377007a7dab02906e55b964caa354e3b9
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-08 23:13:33 +0100

    Rewrote task 2 for a new deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to my web servers, using the function do_deploy:
 """
 
-from fabric.api import put, run, env
-from os.path import exists
 import os
+from fabric.api import *
+
+env.user = "ubuntu"
 env.hosts = ['18.209.180.49', '54.87.207.177']
-env.user = 'ubuntu'
 env.password = os.environ['password']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+    Deploys an archive file to the server
+    Args:
+        archive_path: path to the archive file on the local machine
+                        to be deployed to the server
+    Return:
+        True: on success
+        False: if otherwise
+    """
+
+    if not os.path.exists(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except Exception:
-        return False
+    put(archive_path, '/tmp/')
+    split_slash = archive_path.split("/")[-1]
+    remove_tgz = split_slash.split(".")[0]
+    directory = '/data/web_static/releases/'
+    run('mkdir -p {}{}'.format(directory, remove_tgz))
+    run('tar -xzf /tmp/{0}.tgz -C {1}{0}'.format(remove_tgz, directory))
+    run('rm /tmp/{}.tgz'.format(remove_tgz))
+    run('mv {0}{1}/web_static/* {0}{1}'.format(directory, remove_tgz))
+    run('rm -rf {}{}/web_static'.format(directory, remove_tgz))
+    run('rm -rf /data/web_static/current')
+    run('ln -s {}{} /data/web_static/current'.format(directory, remove_tgz))
