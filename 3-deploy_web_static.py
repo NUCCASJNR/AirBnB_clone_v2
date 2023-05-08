369ea640275685525920ee171f7253ae6aaae7da
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-08 12:32:26 +0100

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -24,28 +24,26 @@ def do_pack():
     except Exception as a:
         return None
 
-
 def do_deploy(archive_path):
-    """Distribute an archive to the web servers"""
-
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+       # print(file_n)
+       # print(no_ext)
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        splited = archive_path.split(".")[0].split("/")[-1]
-        run("sudo mkdir -p /data/web_static/releases/{}".format(splited))
-        run("sudo tar -xzvf /tmp/{}.tgz -C\
-                /data/web_static/releases/{}/".format(splited, splited))
-        run("sudo rm /tmp/{}.tgz".format(splited))
-        run("sudo mv /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}".format(splited, splited))
-        run("sudo rm -rf /data/web_static/releases/\
-                {}/web_static/".format(splited))
-        run(f"sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}\
-                /data/web_static/current".format(splited))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
+    except Exception:
         return False
 
 
@@ -53,5 +51,5 @@ def deploy():
     """Deploys a full archive to the server"""
     file = do_pack()
     if file is None:
-        return none
+        return False
     return do_deploy(file)
