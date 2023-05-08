6edb151731318b02c8e8ced83d119c7397135d0d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-08 23:35:53 +0100

    hmmmmm...ohkoo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,12 +34,6 @@ env.password = os.environ['password']
 def do_deploy(archive_path):
     """
     Deploys an archive file to the server
-    Args:
-        archive_path: path to the archive file on the local machine
-                        to be deployed to the server
-    Return:
-        True: on success
-        False: if otherwise
     """
 
     if not os.path.exists(archive_path):
