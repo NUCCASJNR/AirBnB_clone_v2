0c70c26892fdf4f9df0c055fcc03737382edfc06
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-19 11:23:25 +0100

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ This script that starts a Flask web application
 
 
 from flask import Flask
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
@@ -20,4 +20,7 @@ def hello():
         Hello HBNB
     """
 
-    return f'Hello HBNB!'
+    return ("Hello HBNB!")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
