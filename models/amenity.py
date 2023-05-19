bcf7ed3742a9774ead5e4f56788f02ec06eaad68
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-19 22:21:12 +0100

    pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -5,10 +5,11 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from os import getenv
 
+
 class Amenity(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128),
-                nullable=False)
+                      nullable=False)
     else:
         name = ""
