a6c81a327151620ee9d82fd808cf99c22fed794d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-20 23:19:04 +0100

    omoh

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,6 +17,7 @@ def close_db():
     """
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def state():
     """
@@ -24,6 +25,7 @@ def state():
     """
     state = storage.all(State)
     return render_template("7-states_list.html", state=state)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
