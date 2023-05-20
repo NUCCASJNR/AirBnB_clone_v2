2894d7cb69e7c224b66bfde9fd7e9fdb49e4ce6b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-20 23:15:37 +0100

    try

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from os import getenv
+#from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -11,5 +12,6 @@ class Amenity(BaseModel, Base):
         __tablename__ = 'amenities'
         name = Column(String(128),
                       nullable=False)
+#        place_amenities = relationship("Place", secondary=place_amenity)
     else:
         name = ""
--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
-    else:
+    elif  getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             city_list = []
